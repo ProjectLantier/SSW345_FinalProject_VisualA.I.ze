c60e8162e02399f21ab251b78c8e1a6ee834b6dc
rdhadda123 <rdhadda@stevens.edu>
2023-05-01 23:09:56 -0400

    Changes for class from PR

--- ImageProcessor.py
+++ ImageProcessor.py
@@ -1,21 +1,36 @@
 class ImageProcessor:
+    """A class for processing images"""
+    
     def __init__(self, image):
+        """Initialize the ImageProcessor object with an image"""
         self.image = image
     
     def detectText(self):
-        # Implementation for text detection
+        """Detect text in the image and return a list of text strings"""
         text_list = []
-        # code to detect text from image and store it in text_list
+        try:
+            # code to detect text from image and store it in text_list
+            pass
+        except Exception as e:
+            print(f"Error detecting text: {str(e)}")
         return text_list
     
     def detectObjects(self):
-        # Implementation for object detection
+        """Detect objects in the image and return a list of object strings"""
         object_list = []
-        # code to detect objects from image and store it in object_list
+        try:
+            # code to detect objects from image and store it in object_list
+            pass
+        except Exception as e:
+            print(f"Error detecting objects: {str(e)}")
         return object_list
     
     def recognizeLandmarks(self):
-        # Implementation for landmark recognition
+        """Recognize landmarks in the image and return a list of landmark strings"""
         landmark_list = []
-        # code to recognize landmarks from image and store it in landmark_list
+        try:
+            # code to recognize landmarks from image and store it in landmark_list
+            pass
+        except Exception as e:
+            print(f"Error recognizing landmarks: {str(e)}")
         return landmark_list
