b58c116ad697ce0559f52c5dc9bb2b9425925711
rdhadda123 <rdhadda@stevens.edu>
2023-05-01 22:43:52 -0400

    Image Processor Class

---
+++ ImageProcessor.py
@@ -0,0 +1,21 @@
+class ImageProcessor:
+    def __init__(self, image):
+        self.image = image
+    
+    def detectText(self):
+        # Implementation for text detection
+        text_list = []
+        # code to detect text from image and store it in text_list
+        return text_list
+    
+    def detectObjects(self):
+        # Implementation for object detection
+        object_list = []
+        # code to detect objects from image and store it in object_list
+        return object_list
+    
+    def recognizeLandmarks(self):
+        # Implementation for landmark recognition
+        landmark_list = []
+        # code to recognize landmarks from image and store it in landmark_list
+        return landmark_list
